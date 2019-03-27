410809e70b2ed27bff73d4c8585aea5f265b84bf
Chinweezy <ugoji.chinwe@gmail.com>
2019-03-27 01:44:46 +0100

    [#164914470] Added a search filter for an admin to view a specific account based on the account number or first name of user. Also added a button to each account for an adminto delete a user

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ function loadAccPage() {
     bankAccounts.style.display = "table";
 }
 
+function searchAccountRecord() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("dummy-accounts");
+  tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[3];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[i].style.display = "";
+          } else {  
+            tr[i].style.display = "none";
+            }
+        }
+    }
+
+    for (j = 0; j < tr.length; j++) {
+        td = tr[j].getElementsByTagName("td")[0];
+        if (td) {
+          txtValue = td.textContent || td.innerText;
+          if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            tr[j].style.display = "";
+          } else {  
+            tr[j].style.display = "none";
+            }
+        }
+    }    
+}
+
+
+function deleteUser(r) {
+    var i = r.parentNode.parentNode.rowIndex;
+    document.getElementById("dummy-accounts").deleteRow(i);
+}
