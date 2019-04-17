ab0a75b9cc69449924d46196d27c4b4bdd7ccf54
Chinwe <ugoji.chinwe@gmail.com>
2019-04-17 10:42:46 +0100

    chore(unit-test): add unit test for user signup Write unit test for user signup to show what error messages would show when a user omits some fields [Finishes #165233051]

--- test/server/userTest.js
+++ test/server/userTest.js
@@ -1,10 +1,9 @@
-/* eslint-disable import/no-named-as-default-member */
-import chai, { expect } from 'chai';
+import chai from 'chai';
 import chaiHttp from 'chai-http';
-// eslint-disable-next-line import/no-named-as-default
 import app from '../../app';
 
 chai.use(chaiHttp);
+chai.should();
 
 describe('Testing user signup controller', () => {
   it('should register a new user when all the fields are provided', (done) => {
@@ -16,25 +15,16 @@ describe('Testing user signup controller', () => {
         email: 'test@test.com',
         password: 'Welcome@2019',
         confirmPassWord: 'Welcome@2019',
+        type: 'client',
+        isAdmin: false,
       })
-      .end((error, response) => {
-        expect(response.body).to.be.an('object');
-        expect(response).to.have.status(201);
-        expect(response.body.status).to.equal(201);
-        expect(response.body.data).to.be.a('object');
-        expect(response.body.data).to.have.property('token');
-        expect(response.body.data).to.have.property('id');
-        expect(response.body.data).to.have.property('firstName');
-        expect(response.body.data).to.have.property('lastName');
-        expect(response.body.data).to.have.property('email');
-        expect(response.body.data.token).to.be.a('string');
-        expect(response.body.data.id).to.be.a('integer');
-        expect(response.body.data.firstName).to.be.a('string');
-        expect(response.body.data.lastName).to.be.a('string');
-        expect(response.body.data.email).to.be.a('string');
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.body.should.be.a('object');
         done();
       });
   });
+
   it('should return an error when the first name is not given', (done) => {
     chai.request(app)
       .post('/api/v1/auth/signup')
@@ -43,17 +33,15 @@ describe('Testing user signup controller', () => {
         email: 'test@test.com',
         password: 'Welcome@2019',
         confirmPassWord: 'Welcome@2019',
+        type: 'client',
+        isAdmin: false,
       })
-      .end((error, response) => {
-        expect(response.body).to.be.an('object');
-        expect(response).to.have.status(404);
-        expect(response.body.status).to.equal(404);
-        expect(response.body).to.have.property('error');
-        expect(response.body.error).to.be.a('string');
-        expect(response.body.error).to.equal('First name is required');
+      .end((err, res) => {
+        res.should.have.status(400);
         done();
       });
   });
+
   it('should return an error when the last name is not given', (done) => {
     chai.request(app)
       .post('/api/v1/auth/signup')
@@ -62,14 +50,64 @@ describe('Testing user signup controller', () => {
         email: 'test@test.com',
         password: 'Welcome@2019',
         confirmPassWord: 'Welcome@2019',
+        type: 'client',
+        isAdmin: false,
       })
-      .end((error, response) => {
-        expect(response.body).to.be.an('object');
-        expect(response).to.have.status(404);
-        expect(response.body.status).to.equal(404);
-        expect(response.body).to.have.property('error');
-        expect(response.body.error).to.be.a('string');
-        expect(response.body.error).to.equal('Last name is required');
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+
+  it('should return an error when the email is not given', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        firstName: 'chinwe',
+        lastName: 'ugoji',
+        email: 'test@test.com',
+        password: 'Welcome@2019',
+        confirmPassWord: 'Welcome@2019',
+        type: 'client',
+        isAdmin: false,
+      })
+      .end((err, res) => {
+        res.should.have.status(400);
+        done();
+      });
+  });
+
+  it('should return an error when the passwords do not match', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        firstName: 'chinwe',
+        lastName: 'ugoji',
+        email: 'test@test.com',
+        password: 'Welcome@2019',
+        confirmPassWord: 'Welcome@2018',
+        type: 'client',
+        isAdmin: false,
+      })
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+
+  it('should return an error when the type of user is not stated', (done) => {
+    chai.request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        firstName: 'chinwe',
+        lastName: 'ugoji',
+        email: 'test@test.com',
+        password: 'Welcome@2019',
+        confirmPassWord: 'Welcome@2018',
+        isAdmin: false,
+      })
+      .end((err, res) => {
+        res.should.have.status(404);
         done();
       });
   });
